6745d8face0863982663a376e4fd784fb2295c8b
Oleg <oleg.smal@vivino.com>
2023-06-08 18:45:41 +0300

    subtests

--- sdk/sdk_test.go
+++ sdk/sdk_test.go
@@ -62,8 +62,8 @@ func TestAnthropic_DoPrompt(t *testing.T) {
 }
 
 func TestAnthropic_Do(t *testing.T) {
-	art := assert.New(t)
 	type test struct {
+		name     string
 		response []byte
 		status   int
 		prompt   string
@@ -72,8 +72,8 @@ func TestAnthropic_Do(t *testing.T) {
 		expected error
 	}
 	tests := []test{
-		// Common case.
 		{
+			name: "common case",
 			response: []byte(`{
 				"completion":" The sky appears blue to our eyes due to the way the atmosphere interacts with sunlight.",
 				"stop_reason":"stop_sequence"
@@ -84,8 +84,8 @@ func TestAnthropic_Do(t *testing.T) {
 			tokens:   255,
 			expected: nil,
 		},
-		// Internal anthropic error.
 		{
+			name: "internal anthropic error",
 			response: []byte(`{
 				"type": "invalid_request_error",
 				"message":"field required"
@@ -96,8 +96,8 @@ func TestAnthropic_Do(t *testing.T) {
 			tokens:   0,
 			expected: ErrInternalAnthropic,
 		},
-		// Invalid prompt format.
 		{
+			name:     "internal prompt format",
 			response: []byte(`{}`),
 			status:   0,
 			prompt:   "Why is the sky blue?",
@@ -108,61 +108,67 @@ func TestAnthropic_Do(t *testing.T) {
 	}
 
 	for _, tt := range tests {
-		client.RespondWith(tt.response, tt.status, nil)
-		resp, err := anthropic.Do(Request{
-			Prompt:            tt.prompt,
-			Model:             tt.model,
-			MaxTokensToSample: tt.tokens,
+		t.Run(tt.name, func(_t *testing.T) {
+			art := assert.New(_t)
+			client.RespondWith(tt.response, tt.status, nil)
+			resp, err := anthropic.Do(Request{
+				Prompt:            tt.prompt,
+				Model:             tt.model,
+				MaxTokensToSample: tt.tokens,
+			})
+			if tt.expected != nil {
+				if art.Error(err) {
+					art.ErrorContains(err, tt.expected.Error())
+					art.Nil(resp)
+				}
+			} else {
+				if art.NoError(err) {
+					art.NotNil(resp)
+				}
+			}
 		})
-		if tt.expected != nil {
-			if art.Error(err) {
-				art.ErrorContains(err, tt.expected.Error())
-				art.Nil(resp)
-			}
-		} else {
-			if art.NoError(err) {
-				art.NotNil(resp)
-			}
-		}
 	}
 }
 
 func TestAnthropic_ValidatePrompt(t *testing.T) {
-	art := assert.New(t)
 	type test struct {
-		in  string
-		out error
+		name string
+		in   string
+		out  error
 	}
 	tests := []test{
 		{
-			// Common case.
-			in:  "\n\nHuman: abc123\n\nAssistant:",
-			out: nil,
+			name: "common case",
+			in:   "\n\nHuman: abc123\n\nAssistant:",
+			out:  nil,
 		},
 		{
-			// Multiline message.
-			in:  "\n\nHuman: abc123\nabc123\n\nAssistant:",
-			out: nil,
+			name: "multiline message",
+			in:   "\n\nHuman: abc123\nabc123\n\nAssistant:",
+			out:  nil,
 		},
 		{
-			// Empty message.
-			in:  "\n\nHuman: \n\nAssistant:",
-			out: nil,
+			name: "empty message",
+			in:   "\n\nHuman: \n\nAssistant:",
+			out:  nil,
 		},
 		{
-			// Non formatted message.
-			in:  "abc123",
-			out: ErrInvalidPromptFormat,
+			name: "non formatted message",
+			in:   "abc123",
+			out:  ErrInvalidPromptFormat,
 		},
 		{
-			// Empty prompt.
-			in:  "",
-			out: ErrInvalidPromptFormat,
+			name: "empty prompt",
+			in:   "",
+			out:  ErrInvalidPromptFormat,
 		},
 	}
 
 	for _, tt := range tests {
-		out := anthropic.validatePrompt(tt.in)
-		art.Equal(tt.out, out)
+		t.Run(tt.name, func(_t *testing.T) {
+			art := assert.New(_t)
+			out := anthropic.validatePrompt(tt.in)
+			art.Equal(tt.out, out)
+		})
 	}
 }
